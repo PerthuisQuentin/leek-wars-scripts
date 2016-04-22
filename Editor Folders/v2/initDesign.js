b47563c803e42f121f2e7abbf8aab9104408253e
Ebatsin <bast.esca@gmail.com>
2016-04-22 11:00:42 +0200

    removed editor folder

--- Editor Folders/v2/initDesign.js
+++
@@ -1,8 +0,0 @@
-// on charge la page. On annule tous ce qui pourrait être fait
-LW.on('pageload', function() {
-	if(LW.currentPage === "editor") {
-		LW.pages.editor.init = function(){
-			console.log('Redirection editeur en cours');
-		};
-	}
-});
