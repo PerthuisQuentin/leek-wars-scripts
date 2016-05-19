ac61ff9256741c39b55564abba648ddc9540b8e5
Ebatsin <bast.esca@gmail.com>
2016-05-19 22:54:25 +0200

    fixed access to array instead of element

--- Chat Link Preview/chat_link_preview.user.js
+++ Chat Link Preview/chat_link_preview.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          [Leek Wars] Chat link preview
 // @namespace     https://github.com/Ebatsin/Leek-Wars/
-// @version       0.43.1
+// @version       0.43.2
 // @description   Permet d'afficher une preview des ressources linkées dans le chat
 // @author        Twilight
 // @projectPage   https://github.com/Ebatsin/Leek-Wars/
@@ -249,7 +249,7 @@ function getMaxMediaHeight() {
 				'displayMethod': 'basic-image',
 				'data': {
 					'title': 'Prntscr - Image',
-					'url': imgUrl.replace(/https?/, 'https')
+					'url': imgUrl[0].replace(/https?/, 'https')
 				}
 			};
 		},
