d42dba7dbf0d09bf39fd098df4c87dc575660bc1
Ebatsin <bast.esca@gmail.com>
2016-09-11 19:05:40 +0200

    Add the option to go directly to the reportand skip the fight

--- EditorFightNewTab/editor-fight-new-tab.user.js
+++ EditorFightNewTab/editor-fight-new-tab.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           [Leek Wars] Editor-fight - new tab
 // @namespace      https://github.com/Ebatsin/Leek-Wars/
-// @version        0.2.1
+// @version        0.3
 // @description    Lance les tests dans un nouvel onglet
 // @author         Twilight
 // @projectPage    https://github.com/Ebatsin/Leek-Wars/
@@ -12,15 +12,33 @@
 // ==/UserScript==
 
 (function() {
+	var loaded = false; // the editor page has already been loaded
+
 	function init() {
 		if(LW.currentPage == undefined) {
 			setTimeout(init, 200);
 			return;
 		}
+
+		if(!localStorage.getItem('EFNT-showreport')) {
+			localStorage.setItem('EFNT-showreport', false);
+		}
+
 		LW.on('pageload', function() {
 			if(LW.currentPage == 'editor') {
-				replace();
-				
+				setTimeout(replace, 200);
+				return;
+			}
+
+			if(LW.currentPage === 'settings') {
+				$('#settings-page .flex-container')
+				.first()
+				.append('<div class="column6"><div class="panel"><div class="header"><h2>[Userscript] Editor Fight New Tab</h2></div><div class="content"><input type="checkbox" ' + 
+					(localStorage.getItem('EFNT-showreport') === 'true' ? 'checked="true"' : '') + ' id="EFNT-report"><label for="EFNT-report"> Afficher directement le rapport ?</label></div></div></div>');
+ 				
+ 				document.querySelector('#EFNT-report').addEventListener('change', function() {
+ 					localStorage.setItem('EFNT-showreport', this.checked);
+ 				});
 			}
 		});
 	}
@@ -29,11 +47,23 @@
 		// only generated if a test is launched, just wait for it to be opened
 		var old = document.querySelector('#launch');
 		if(old == null) {
-			setTimeout(replace, 400);
+			setTimeout(replace, 100);
+			return;
+		}
+		else if(loaded) {
+			old.id = ''; // prevent the modification of the old element and wait for the new one to be created
+			setTimeout(replace, 100);
+			loaded = false;
 			return;
 		}
 		var clone = old.cloneNode(true);
 		old.parentNode.replaceChild(clone, old);
+		loaded = true;
+		var center = document.querySelector('.test_popup .content center');
+		center.innerHTML += '<label style="margin-left: 20px;"><input type="checkbox" ' + (localStorage.getItem('EFNT-showreport') === 'true' ? 'checked="true"' : '') + ' id="EFNT-report2"> Afficher rapport</label>';
+		document.querySelector('#EFNT-report2').addEventListener('change', function() {
+ 			localStorage.setItem('EFNT-showreport', this.checked);
+ 		});
 
 		// application de notre event perso
 		clone.addEventListener('click', function() {
@@ -52,7 +82,7 @@
 					_testPopup.dismiss();
 					// create dummy link
 					var link = document.createElement('a');
-					link.setAttribute('href', 'https://leekwars.com/fight/' + data.fight);
+					link.setAttribute('href', (localStorage.getItem('EFNT-showreport') === 'true' ? 'https://leekwars.com/report/' : 'https://leekwars.com/fight/') + data.fight);
 					link.setAttribute('target', '_blank');
 					link.style.position = "absolute";
 					link.style.left = "-1000px";
