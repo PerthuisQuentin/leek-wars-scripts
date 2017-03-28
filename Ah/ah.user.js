48289f8373921cd57fc5a735c8408dc32db76112
PerthuisQuentin <perthuis.quentin@gmail.com>
2017-03-28 21:05:43 +0200

    :sparkles: [Ah] Allow multiple 'Ah !'

--- Ah/ah.user.js
+++ Ah/ah.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [Leek Wars] Ah !
 // @namespace    https://github.com/PerthuisQuentin/Leek-Wars-Scripts/
-// @version      0.2.1
+// @version      0.3.0
 // @description  Déclenche un "Ah !" sonore lors d'un "Ah !" dans le chat
 // @author       TheTintin
 // @projectPage   https://github.com/PerthuisQuentin/Leek-Wars-Scripts/
@@ -14,12 +14,17 @@
     'use strict';
 
     var audio = new Audio('https://raw.githubusercontent.com/PerthuisQuentin/Leek-Wars-Scripts/master/Ah/ah.wav');
-    var regex = new RegExp(/ah\s?!/i);
+    var regex = new RegExp(/(ah\s?!)/i);
 
     function check(message) {
-        if(!regex.test(message)) return;
+    	var count = message.match(regex).length;
 
-        audio.play();
+    	if(!count) return;
+
+        (function loop() {
+            audio.play(); count--;
+        	if(count > 0) setTimeout(loop, 500);
+    	})();
     }
 
     LW.on('pageload', function() {
