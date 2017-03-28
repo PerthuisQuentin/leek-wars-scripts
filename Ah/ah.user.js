9d1760321fc8c89b66414b16252b8c270b41bdf8
PerthuisQuentin <perthuis.quentin@gmail.com>
2017-03-28 20:38:56 +0200

    :fire: [Ah] Cleaning

--- Ah/ah.user.js
+++ Ah/ah.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [Leek Wars] Ah !
 // @namespace    https://github.com/PerthuisQuentin/Leek-Wars-Scripts/
-// @version      0.1
+// @version      0.2
 // @description  Déclenche un "Ah !" sonore lors d'un "Ah !" dans le chat
 // @author       TheTintin
 // @projectPage   https://github.com/PerthuisQuentin/Leek-Wars-Scripts/
@@ -13,21 +13,27 @@
 (function() {
     'use strict';
 
+    var audio = new Audio('https://raw.githubusercontent.com/PerthuisQuentin/Leek-Wars-Scripts/master/Ah/ah.wav');
+    var regex = new RegExp("/ah\s?!/i");
+
+    function check(message) {
+        if(!regex.test(message)) return;
+
+        audio.play();
+    }
+
     LW.on('pageload', function() {
-        var audio_ah = new Audio('https://raw.githubusercontent.com/PerthuisQuentin/Leek-Wars-Scripts/master/Ah/ah.wav');
-        var audio_taisez_vous = new Audio('https://raw.githubusercontent.com/PerthuisQuentin/Leek-Wars-Scripts/master/Ah/taisez_vous.mp3');
-        var next = LW.chat.receive;
+        var chatReceive = LW.chat.receive;
+        var messageReceive = LW.messages.receive;
 
         LW.chat.receive = function(data) {
-            if(data[3] === "Ah !" || data[3] === "ah !" || data[3] === "ah!" || data[3] === "Ah!" || data[3] === "AH!" || data[3] === "AH !") {
-                audio_ah.play();
-            }
-            
-            if(data[3] === "Taisez-vous !" || data[3] === "taisez-vous !" || data[3] === "Taisez-vous" || data[3] === "taisez-vous"){
-                audio_taisez_vous.play();   
-            }
+            check(data[3]);
+            chatReceive(data);
+        };
 
-            next(data);
+        LW.messages.receive = function(data) {
+            check(data.message);
+            messageReceive(data);
         };
     });
 })();
